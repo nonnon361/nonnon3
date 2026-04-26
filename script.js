85a407c9fe385d528f42886a91c7a504e7f1011e
nonnon361 <127860110+nonnon361@users.noreply.github.com>
2026-04-26 07:29:57 +0000

    add click event

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 const input = document.getElementById("noteInput");
 const button = document.getElementById("addBtn");
 const notes = document.getElementById("notes");
+button.addEventListener("click", () => {
+  console.log("clicked");
+});
